e24ebcc8018f9855ccd29df8288c6f6f73e78f7f
EthanPan233 <ethan@LAPTOP-RJQ46E9D.localdomain>
2022-03-03 09:56:33 -0800

    add draw_hyper_image.py

---
+++ scripts/draw_hyper_image.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+import sys
+from pathlib import Path
+
+PATH = Path(sys.argv[1])
+
+for dir in PATH.glob("**"):
+	designName = str(dir).split("/")[-1]
+	designName
